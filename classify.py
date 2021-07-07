fe254968c05cce4c68a9f6b75f2f5e49c1c2b096
SYSTEM SEMICONDUCTOR ENGINEERING FOR AI <77914487+ssai-snu@users.noreply.github.com>
2021-07-07 20:48:59 +0900

    Add files via upload

---
+++ classify.py
@@ -0,0 +1,54 @@
+import argparse
+import time
+
+from PIL import Image
+from pycoral.adapters import classify
+from pycoral.adapters import common
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-m', '--model', required=True,
+                      help='File path of .tflite file.')
+  parser.add_argument('-i', '--input', required=True,
+                      help='Image to be classified.')
+  parser.add_argument('-l', '--labels',
+                      help='File path of labels file.')
+  parser.add_argument('-k', '--top_k', type=int, default=1,
+                      help='Max number of classification results')
+  parser.add_argument('-t', '--threshold', type=float, default=0.0,
+                      help='Classification score threshold')
+  parser.add_argument('-c', '--count', type=int, default=5,
+                      help='Number of times to run inference')
+  args = parser.parse_args()
+
+  labels = read_label_file(args.labels) if args.labels else {}
+
+  interpreter = make_interpreter(args.model)
+  interpreter.allocate_tensors()
+
+  size = common.input_size(interpreter)
+  image = Image.open(args.input).resize(size, Image.ANTIALIAS)
+  common.set_input(interpreter, image)
+
+  print('----INFERENCE TIME----')
+  print('Note: The first inference on Edge TPU is slow because it includes',
+        'loading the model into Edge TPU memory.')
+  for _ in range(args.count):
+    start = time.perf_counter()
+    interpreter.invoke()
+    inference_time = time.perf_counter() - start
+    classes = classify.get_classes(interpreter, args.top_k, args.threshold)
+    print('%.1fms' % (inference_time * 1000))
+
+  print('-------RESULTS--------')
+  for c in classes:
+    print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+
+
+if __name__ == '__main__':
+  main()
+
+
