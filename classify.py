5adbabf33210112fe86d7181538512d2019804ee
SYSTEM SEMICONDUCTOR ENGINEERING FOR AI <77914487+ssai-snu@users.noreply.github.com>
2021-07-07 21:11:41 +0900

    Add files via upload

--- classify.py
+++ classify.py
@@ -1,54 +1,75 @@
 import argparse
+import gstreamer
+import os
 import time
 
-from PIL import Image
-from pycoral.adapters import classify
-from pycoral.adapters import common
+from common import avg_fps_counter, SVG
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
+from pycoral.adapters.common import input_size
+from pycoral.adapters.classify import get_classes
+
+def generate_svg(size, text_lines):
+    svg = SVG(size)
+    for y, line in enumerate(text_lines, start=1):
+      svg.add_text(10, y * 20, line, 20)
+    return svg.finish()
 
 def main():
-  parser = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('-m', '--model', required=True,
-                      help='File path of .tflite file.')
-  parser.add_argument('-i', '--input', required=True,
-                      help='Image to be classified.')
-  parser.add_argument('-l', '--labels',
-                      help='File path of labels file.')
-  parser.add_argument('-k', '--top_k', type=int, default=1,
-                      help='Max number of classification results')
-  parser.add_argument('-t', '--threshold', type=float, default=0.0,
-                      help='Classification score threshold')
-  parser.add_argument('-c', '--count', type=int, default=5,
-                      help='Number of times to run inference')
-  args = parser.parse_args()
+    default_model_dir = './'
+    default_model = 'mnist_model.tflite'
+    default_labels = 'labels.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='.tflite model path',
+                        default=os.path.join(default_model_dir,default_model))
+    parser.add_argument('--labels', help='label file path',
+                        default=os.path.join(default_model_dir, default_labels))
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--threshold', type=float, default=0.1,
+                        help='classifier score threshold')
+    parser.add_argument('--videosrc', help='Which video source to use. ',
+                        default='/dev/video0')
+    parser.add_argument('--headless', help='Run without displaying the video.',
+                        default=False, type=bool)
+    parser.add_argument('--videofmt', help='Input video format.',
+                        default='raw',
+                        choices=['raw', 'h264', 'jpeg'])
+    args = parser.parse_args()
 
-  labels = read_label_file(args.labels) if args.labels else {}
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
+    labels = read_label_file(args.labels)
+    inference_size = input_size(interpreter)
 
-  interpreter = make_interpreter(args.model)
-  interpreter.allocate_tensors()
+    # Average fps over last 30 frames.
+    fps_counter = avg_fps_counter(30)
 
-  size = common.input_size(interpreter)
-  image = Image.open(args.input).resize(size, Image.ANTIALIAS)
-  common.set_input(interpreter, image)
+    def user_callback(input_tensor, src_size, inference_box):
+      nonlocal fps_counter
+      start_time = time.monotonic()
+      run_inference(interpreter, input_tensor)
 
-  print('----INFERENCE TIME----')
-  print('Note: The first inference on Edge TPU is slow because it includes',
-        'loading the model into Edge TPU memory.')
-  for _ in range(args.count):
-    start = time.perf_counter()
-    interpreter.invoke()
-    inference_time = time.perf_counter() - start
-    classes = classify.get_classes(interpreter, args.top_k, args.threshold)
-    print('%.1fms' % (inference_time * 1000))
+      results = get_classes(interpreter, args.top_k, args.threshold)
+      end_time = time.monotonic()
+      text_lines = [
+          ' ',
+          'Inference: {:.2f} ms'.format((end_time - start_time) * 1000),
+          'FPS: {} fps'.format(round(next(fps_counter))),
+      ]
+      for result in results:
+          text_lines.append('score={:.2f}: {}'.format(result.score, labels.get(result.id, result.id)))
+      print(' '.join(text_lines))
+      return generate_svg(src_size, text_lines)
 
-  print('-------RESULTS--------')
-  for c in classes:
-    print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
-
+    result = gstreamer.run_pipeline(user_callback,
+                                    src_size=(640, 480),
+                                    appsink_size=inference_size,
+                                    videosrc=args.videosrc,
+                                    videofmt=args.videofmt,
+                                    headless=args.headless)
 
 if __name__ == '__main__':
-  main()
-
-
+    main()
